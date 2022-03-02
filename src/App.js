b1d06e43f7d0072fb3bacf139aa9cad4d7c14247
kk1rA <levashovv.kir@yandex.ru>
2022-03-02 18:20:33 +0300

    added fake rest api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PostList from './components/PostList';
 import FormComponent from './components/FormComponent';
 import './styles/App.css';
@@ -6,16 +6,24 @@ import Postfilter from './components/PostFilter';
 import Modalwindow from './components/UI/ModalWindow/ModalWindow';
 import Unibutton from './components/UI/UniButton';
 import { usePosts } from './hooks/usePosts';
+import PostsService from './API/PostsService';
+import LoaderComponent from './components/UI/LoaderComponent';
+import {useFetching} from './hooks/useFetching';
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'JavaScript', description: 'Language'},
-    {id: 2, title: 'Swift', description: 'Apple'},
-    {id: 3, title: 'Python', description: 'Python'},
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
   const searchedPosts = usePosts(posts, filter.sort, filter.query);
+  
+  const [fetchPosts, isLoadingPosts, errorPostLoading] = useFetching( async () => {
+    const response = await PostsService.getAll();
+    setPosts(response);
+  })
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -36,7 +44,11 @@ function App() {
       </Modalwindow>
       <hr style={{marginTop: '20px', marginBottom: '20px'}}/>
       <Postfilter filter={filter} setFilter={setFilter}/>
-      <PostList deletePost={deletePost} posts={searchedPosts} title="POSTS List"/>
+      { errorPostLoading && <h1 style={{marginTop: 20, display: 'flex', justifyContent: 'center'}}>errorPostLoading</h1>}
+      { isLoadingPosts 
+        ? <LoaderComponent/>
+        : <PostList deletePost={deletePost} posts={searchedPosts} title="POSTS List"/>
+      }
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -6,7 +6,7 @@ const PostItem = (props) => {
     <div className='post'>
         <div className='post__content'>
           <strong>{props.number}. {props.post.title}</strong>
-          <div>{props.post.description}</div>
+          <div>{props.post.body}</div>
         </div>
         <div>
           <Unibutton onClick={() => props.delete(props.post)} title='Delete content'>Delete</Unibutton>
